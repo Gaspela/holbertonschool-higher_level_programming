9690082b7438ebc9d1c1ae1fe1ee4f9235162f4c
Gaspela04 <samirmillanorozco@hotmail.com>
2020-01-17 16:31:10 +0000

    allfix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -8,19 +8,6 @@ class Rectangle:
         self.height = height
 
     @property
-    def height(self):
-        """Return height"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise TypeError("height must be >= 0")
-        self.__height = value
-
-    @property
     def width(self):
         """Return width"""
         return self.__width
@@ -30,8 +17,21 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
-            raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        """Return height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         """return area"""
