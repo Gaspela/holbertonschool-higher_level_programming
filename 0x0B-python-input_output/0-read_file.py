fb184e1133fa79fa8ad6fe30ad2931dcad88a87b
Gaspela04 <samirmillanorozco@hotmail.com>
2020-01-22 16:50:38 +0000

    Number lines used

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,7 @@
 def read_file(filename=""):
     """
     Write a function that reads a text file
-    (UTF8) and prints it to stdout:
+    (UTF8) and prints it to stdout.
     """
     with open(filename, mode="r", encoding="utf-8") as myFile:
         print(myFile.read(), end="")
