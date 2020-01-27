f968a4b7c615c7d6fb5ee2de8038289d2e9abeb4
Gaspela04 <samirmillanorozco@hotmail.com>
2020-01-27 20:28:08 +0000

    Str

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,3 +10,11 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    def __str__(self):
+        """
+        [Square] (<id>) <x>/<y> - <size>
+        """
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
+
+    
