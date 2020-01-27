ade46fcfc2055a483e7eec334718f7f9970bd96f
Gaspela04 <samirmillanorozco@hotmail.com>
2020-01-27 22:52:25 +0000

    Updatesq

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,4 +17,36 @@ class Square(Rectangle):
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
 
-    
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        adding the public method def update
+        (self, *args, **kwargs) that assigns attributes
+        """
+        if args:
+            for i, value in enumerate(args):
+                if i == 0:
+                    self.id = value
+                elif i == 1:
+                    self.size = value
+                elif i == 2:
+                    self.x = value
+                elif i == 3:
+                    self.y = value
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key) is True:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+
+        listsq = {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+        return listsq
