b69202363c90a19f33bfc543c314a6ef71d99c8b
Gaspela04 <samirmillanorozco@hotmail.com>
2020-03-02 18:57:16 +0000

    Full create

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,17 @@
+-- creates a table called second_table.
+-- second_table:
+-- id INT.
+-- name VARCHAR(256).
+-- score INT.
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+
+INSERT INTO
+	second_table (id, name, score)
+VALUES
+	(1, "jhon", 10),
+
+(2, "Alex", 3),
+
+(3, "Bob", 14),
+
+(4, "George", 8);
