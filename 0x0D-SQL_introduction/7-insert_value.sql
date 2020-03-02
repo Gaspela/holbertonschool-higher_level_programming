5c829ad41710e179f00833f307ff28e7190514d0
Gaspela04 <samirmillanorozco@hotmail.com>
2020-03-02 19:29:48 +0000

    Fix sintax

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,10 +1,10 @@
 -- inserts a new row in the table first_table.
-INSERT INTO first_table 
-( -- columns to insert data into
- id, name
-)
+INSERT INTO
+    first_table (-- columns to insert data into
+    id, name)
 VALUES
-( -- first row: values for the columns in the list above
- 89, "Holberton School"
-)
-
+    (
+        -- first row: values for the columns in the list above
+        89,
+        "Holberton School"
+    )
