480031757765a5e4238f7fcad6cd1ac4b2a8c395
Samir millan orozco <samirmillanorozco@hotmail.com>
2020-04-13 16:13:07 +0000

    fix total lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Takes in a URL and displays all HTTP methods the server will accept.
-# -s, --silent
-# -I, --head
-# "$1" Number parmeters received
-# -i, --include
-# cut -d "delimitador"
-# -f (field)
+# Takes in a URL and displays all HTTP methods the server will accept. -s, --silent / -I, --head / "$1" Number parmeters received / -i, --include / cut -d "delimitador" / -f (field)
 curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f2-
