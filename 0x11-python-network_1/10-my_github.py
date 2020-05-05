5c0a5a2de41ce913e942fc1e14fad72a90f64667
Samir millan orozco <samirmillanorozco@hotmail.com>
2020-05-05 17:09:25 +0000

    Next topict

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,7 +7,7 @@ from sys import argv
 
 if __name__ == "__main__":
     r = requests.get(
-            'https://api.github.com/user', auth=(argv[1], argv[2]))
+        'https://api.github.com/user', auth=(argv[1], argv[2]))
     try:
         jfile = r.json()
         print(jfile['id'])
