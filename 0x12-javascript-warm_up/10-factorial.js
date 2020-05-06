ad5867298b746d31f5bdf55582b07bcf991529fe
Samir millan orozco <samirmillanorozco@hotmail.com>
2020-05-06 02:16:15 +0000

    Fix dent

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 function factorial (a) {
-    if (!a || a < 2) {
-      return 1;
-    }
-    return a * factorial(a - 1);
+  if (!a || a < 2) {
+    return 1;
   }
-  
-  console.log(factorial(parseInt(process.argv[2])));
+  return a * factorial(a - 1);
+}
+
+console.log(factorial(parseInt(process.argv[2])));
