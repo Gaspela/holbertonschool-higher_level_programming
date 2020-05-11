88222b9c9fafedb6670c2329a3a374c4bb0e1dfc
Samir millan orozco <samirmillanorozco@hotmail.com>
2020-05-11 20:38:49 +0000

    Objects js

--- 0x13-javascript_objects_scopes_closures/main.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const converter = require('./10-converter').converter;
-
-const myConverter = converter(20);
-
-console.log(myConverter(5));
-console.log(myConverter(10));
-console.log(myConverter(100));
