b3c3abbeaa18862ab021b69717cb54c6581bd9ea
Samir millan orozco <samirmillanorozco@hotmail.com>
2020-05-12 17:20:11 +0000

    Url fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/:id' + process.argv[2];
 request(url, function (err, resp, body) {
   if (err) throw err;
   console.log(JSON.parse(body).title);
